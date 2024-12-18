e38d40bb3ad597055cb3c1735fa3be1b208105e7
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-18 21:25:48 +0300

    Add new actions to habitSlice

--- src/features/habit/habitSlice.ts
+++ src/features/habit/habitSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice, nanoid } from "@reduxjs/toolkit";
 import { HabitsState } from "../../types/types";
 
 const initialState: HabitsState = {
@@ -9,10 +9,39 @@ export const habitSlice = createSlice({
   name: "habit",
   initialState,
   reducers: {
-    addHabit: (state, action) => {},
+    addHabit: (state, action: PayloadAction<HabitsState>) => {
+      state.habits.push({
+        id: nanoid().toString(),
+        name: action.payload.habitName,
+        frequency: action.payload.frequency,
+        completedDates: [],
+        createdAt: new Date().toISOString(),
+      });
+    },
+    toggleHabit: (
+      state,
+      action: PayloadAction<{ id: String; date: string }>
+    ) => {
+      const habit = state.habits.find(
+        (habit) => habit.id === action.payload.id
+      );
+      if (habit) {
+        const index = habit.completedDates.indexOf(action.payload.date);
+        if (index > -1) {
+          habit.completedDates.splice(index, 1);
+        } else {
+          habit.completedDates.push(action.payload.date);
+        }
+      }
+    },
+    removeHabit: (state, action: PayloadAction<String>) => {
+      state.habits = state.habits.filter(
+        (habit) => habit.id !== action.payload
+      );
+    },
   },
 });
 
-export const { addHabit } = habitSlice.actions;
+export const { addHabit, removeHabit, toggleHabit } = habitSlice.actions;
 
 export default habitSlice.reducer;
