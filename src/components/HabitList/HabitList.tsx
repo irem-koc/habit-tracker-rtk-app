4784bbb30f411345654dce1dd5b3065e58b9a218
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-18 21:25:42 +0300

    Add HabitList component

---
+++ src/components/HabitList/HabitList.tsx
@@ -0,0 +1,27 @@
+import Box from "@mui/material/Box/Box";
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import HabitItem from "../HabitItem/HabitItem";
+
+type Props = {};
+
+const HabitList = (props: Props) => {
+  const { habits } = useSelector<RootState>((state) => state.habit);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 3,
+      }}
+    >
+      {habits.map((habit) => (
+        <HabitItem {...habit} />
+      ))}
+    </Box>
+  );
+};
+
+export default HabitList;
